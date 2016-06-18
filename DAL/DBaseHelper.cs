49a884e8092a7da3d9e4e7543526d32fcaad72e2
Ojvar <MOjvar.AmirHossein>
2016-06-18 14:12:13 +0430

    fix bug, in async operation , dbasehepler/execute crashed

--- DAL/DBaseHelper.cs
+++ DAL/DBaseHelper.cs
@@ -14,6 +14,13 @@ namespace BaseDAL
 	/// </summary>
 	public static class DBaseHelper
 	{
+	#region Variables
+		/// <summary>
+		/// Sync Object
+		/// </summary>
+		private static object objSync = new object (); 
+	#endregion
+
 	#region Methods
 		/// <summary>
 		/// Check Connection
@@ -66,121 +73,125 @@ namespace BaseDAL
 		public static CommandResult executeCommand (EnumExecuteType executeType, SqlConnection connection, string commandString, bool closeConnection = true, params KeyValuePair[] parameters)
 		{
 			CommandResult	result	= new CommandResult ();
-			const string	C_RET_VALUE	= "@__retValue";
 
-			if (null != connection)
+			lock (objSync)
 			{
-			#region Run Command
-				try
+				const string	C_RET_VALUE	= "@__retValue";
+
+				if (null != connection)
 				{
-					SqlCommand	command		= null;
+				#region Run Command
+					try
+					{
+						SqlCommand	command		= null;
 
-				#region Prepare command
-					// Create command
-					command	= new SqlCommand (commandString, connection);
+					#region Prepare command
+						// Create command
+						command	= new SqlCommand (commandString, connection);
 
-					// Add parameters
-					if (null != parameters)
-						foreach (KeyValuePair key in parameters)
-							if (null != key)
-							{
-								key.value	= (key.value == null ? DBNull.Value : key.value);
-
-								if (!(key.metadata is SqlDbType))
-									command.Parameters.Add (new SqlParameter (key.key, key.value));
-								else
+						// Add parameters
+						if (null != parameters)
+							foreach (KeyValuePair key in parameters)
+								if (null != key)
 								{
-									SqlDbType	dbType	= (SqlDbType)key.metadata;
+									key.value	= (key.value == null ? DBNull.Value : key.value);
 
-									if ((dbType == SqlDbType.Structured) && (key.stuctureTypeName != null))
-										command.Parameters.Add (new SqlParameter (key.key, (SqlDbType)key.metadata) { Value	= key.value, TypeName = key.stuctureTypeName.ToString () });
+									if (!(key.metadata is SqlDbType))
+										command.Parameters.Add (new SqlParameter (key.key, key.value));
 									else
-										command.Parameters.Add (new SqlParameter (key.key, (SqlDbType)key.metadata) { Value	= key.value });
+									{
+										SqlDbType	dbType	= (SqlDbType)key.metadata;
+
+										if ((dbType == SqlDbType.Structured) && (key.stuctureTypeName != null))
+											command.Parameters.Add (new SqlParameter (key.key, (SqlDbType)key.metadata) { Value	= key.value, TypeName = key.stuctureTypeName.ToString () });
+										else
+											command.Parameters.Add (new SqlParameter (key.key, (SqlDbType)key.metadata) { Value	= key.value });
+									}
 								}
-							}
 
 					
-					// Add ReturnValue Paramter
-					command.Parameters.Add (new SqlParameter () {
-						DbType			= DbType.Object,
-						Direction		= ParameterDirection.ReturnValue,
-						ParameterName	= C_RET_VALUE
-					});
-				#endregion
+						// Add ReturnValue Paramter
+						command.Parameters.Add (new SqlParameter () {
+							DbType			= DbType.Object,
+							Direction		= ParameterDirection.ReturnValue,
+							ParameterName	= C_RET_VALUE
+						});
+					#endregion
 
-				#region Open Connection
-					if (connection.State != System.Data.ConnectionState.Open)
-						connection.Open ();
-				#endregion
+					#region Open Connection
+						if (connection.State != System.Data.ConnectionState.Open)
+							connection.Open ();
+					#endregion
 
-				#region Execute
-					switch (executeType)
-					{
-						default:
-						case EnumExecuteType.nonQuery :
-							command.CommandType = CommandType.Text;
-							result.model	= command.ExecuteNonQuery ();
-							break;
+					#region Execute
+						switch (executeType)
+						{
+							default:
+							case EnumExecuteType.nonQuery :
+								command.CommandType = CommandType.Text;
+								result.model	= command.ExecuteNonQuery ();
+								break;
 
-						case EnumExecuteType.scaler :
-							command.CommandType = CommandType.Text;
-							result.model	= command.ExecuteScalar ();
-							break;
+							case EnumExecuteType.scaler :
+								command.CommandType = CommandType.Text;
+								result.model	= command.ExecuteScalar ();
+								break;
 							
-						case EnumExecuteType.reader :
-							command.CommandType = CommandType.Text;
-							result.model	= readerToTable (command.ExecuteReader ());
-							break;
+							case EnumExecuteType.reader :
+								command.CommandType = CommandType.Text;
+								result.model	= readerToTable (command.ExecuteReader ());
+								break;
 
-						case EnumExecuteType.xmlReader :
-							command.CommandType = CommandType.Text;
-							result.model	= command.ExecuteXmlReader ();
-							break;
+							case EnumExecuteType.xmlReader :
+								command.CommandType = CommandType.Text;
+								result.model	= command.ExecuteXmlReader ();
+								break;
 
-						case EnumExecuteType.procedureReader :
-							command.CommandType = CommandType.StoredProcedure;
-							result.model	= command.ExecuteReader ();
-							break;
+							case EnumExecuteType.procedureReader :
+								command.CommandType = CommandType.StoredProcedure;
+								result.model	= command.ExecuteReader ();
+								break;
 
-						case EnumExecuteType.procedureNonQuery :
-							command.CommandType = CommandType.StoredProcedure;
-							result.model	= command.ExecuteNonQuery ();
-							break;
-					}
+							case EnumExecuteType.procedureNonQuery :
+								command.CommandType = CommandType.StoredProcedure;
+								result.model	= command.ExecuteNonQuery ();
+								break;
+						}
 
-					// create result
-					result.extra	= command.Parameters[C_RET_VALUE].Value;
-					if (result.extra is DBNull)
-						result.extra	= null;
-					result.status	= EnumCommandStatus.success;
-					if (result.model is SqlDataReader)
-					{
-						DataTable	dt	= new DataTable ();
+						// create result
+						result.extra	= command.Parameters[C_RET_VALUE].Value;
+						if (result.extra is DBNull)
+							result.extra	= null;
+						result.status	= EnumCommandStatus.success;
+						if (result.model is SqlDataReader)
+						{
+							DataTable	dt	= new DataTable ();
 						
-						dt.Load ((SqlDataReader)result.model);
-						result.model	= dt;
+							dt.Load ((SqlDataReader)result.model);
+							result.model	= dt;
+						}
+					#endregion
+					}
+					catch (Exception ex)
+					{
+						result.status	= EnumCommandStatus.executeFailed;
+						result.model	= ex;
+						result.message	= "Error: " + ex.Message;
+					}
+					finally
+					{
+						// Close Connection
+						if (closeConnection)
+							connection.Close ();
 					}
 				#endregion
 				}
-				catch (Exception ex)
+				else
 				{
-					result.status	= EnumCommandStatus.executeFailed;
-					result.model	= ex;
-					result.message	= "Error: " + ex.Message;
+					result.status	= EnumCommandStatus.connectionFailed;
+					result.model	= null;
+					result.message	= "Error: Connection failed";
 				}
-				finally
-				{
-					// Close Connection
-					if (closeConnection)
-						connection.Close ();
-				}
-			#endregion
-			}
-			else
-			{
-				result.status	= EnumCommandStatus.connectionFailed;
-				result.model	= null;
-				result.message	= "Error: Connection failed";
 			}
 
 			return result;
